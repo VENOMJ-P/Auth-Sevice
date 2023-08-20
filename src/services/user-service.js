5c715ceaeca9cca3f1b919ac5dbfbe19303ad35c
VENOMJ-P <jay63837@gmail.com>
2023-08-20 19:47:17 +0530

    Added function for token and password comparision

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,4 +1,8 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
 const UserRepository=require('../repository/user-repository');
+const {JWT_KEY} = require('../config/serverConfig')
 
 class UserService {
     constructor(){
@@ -33,6 +37,35 @@ class UserService {
             return user;
         } catch (error) {
             console.log("Something went wrong in the service layer;");
+            throw error;
+        }
+    }
+
+    createToken(user) {
+        try {
+            const result = jwt.sign(user,JWT_KEY, {expiresIn: '1h'});
+            return result;
+        } catch (error) {
+            console.log("Something went wrong in token creation");
+            throw error;
+        }
+    }
+
+    verifyToken(token) {
+        try {
+            const response = jwt.verify(token,JWT_KEY);
+            return response;
+        } catch (error) {
+            console.log("Something went wrong in token validation");
+            throw error;
+        }
+    }
+
+    checkPassword(userInputPassword,encryptedPassword){
+        try {
+            return bcrypt.compareSync(userInputPassword,encryptedPassword);
+        } catch (error) {
+            console.log("Something went wrong in password comparison");
             throw error;
         }
     }
