93f1e49a7980f5a625f208ce119f47a399cf7fec
VENOMJ-P <jay63837@gmail.com>
2023-08-22 21:52:27 +0530

    Add validation err

--- src/controller/user-controller.js
+++ src/controller/user-controller.js
@@ -1,140 +1,135 @@
-const UserService = require('../services/user-service');
+const UserService = require("../services/user-service");
 
 const userService = new UserService();
 
-const create = async (req,res) => {
-    try {
-        const user = await userService.create({
-            email:req.body.email,
-            password:req.body.password
-        });
-        return res.status(201).json({
-            success:true,
-            data:user,
-            message: 'Successfull created a new user',
-            err:{}
-        })
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            message: 'Something went wrong',
-            data:{},
-            success:false,
-            err:error
-        });
-    }
-}
+const create = async (req, res) => {
+  try {
+    const user = await userService.create({
+      email: req.body.email,
+      password: req.body.password,
+    });
+    return res.status(201).json({
+      success: true,
+      data: user,
+      message: "Successfull created a new user",
+      err: {},
+    });
+  } catch (error) {
+    return res.status(error.statusCode).json({
+      message: error.message,
+      data: {},
+      success: false,
+      err: error.explanation,
+    });
+  }
+};
 
-const destory = async (req,res) => {
-    try {
-        const response = await userService.destroy(req.params.id);
-        return res.status(200).json({
-            success:true,
-            data:response,
-            message: 'Successfull delete a user',
-            err:{}
-        })
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            message: 'Something went wrong',
-            data:{},
-            success:false,
-            err:error
-        });
-    }
-}
+const destory = async (req, res) => {
+  try {
+    const response = await userService.destroy(req.params.id);
+    return res.status(200).json({
+      success: true,
+      data: response,
+      message: "Successfull delete a user",
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Something went wrong",
+      data: {},
+      success: false,
+      err: error,
+    });
+  }
+};
 
-const get = async (req,res) => {
-    try {
-        const user = await userService.getById(req.params.id);
-        return res.status(200).json({
-            success:true,
-            data:user,
-            message: 'Successfull get user',
-            err:{}
-        })
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            message: 'Something went wrong',
-            data:{},
-            success:false,
-            err:error
-        });
-    }
-}
+const get = async (req, res) => {
+  try {
+    const user = await userService.getById(req.params.id);
+    return res.status(200).json({
+      success: true,
+      data: user,
+      message: "Successfull get user",
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Something went wrong",
+      data: {},
+      success: false,
+      err: error,
+    });
+  }
+};
 
-const signIn = async (req,res) => {
-    try {
-        const user = await userService.signIn(
-            req.body.email,
-            req.body.password
-        );
-        return res.status(201).json({
-            success:true,
-            data:user,
-            message: 'Successfull sign in',
-            err:{}
-        })
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            message: 'Something went wrong',
-            data:{},
-            success:false,
-            err:error
-        });
-    }
-}
+const signIn = async (req, res) => {
+  try {
+    const user = await userService.signIn(req.body.email, req.body.password);
+    return res.status(201).json({
+      success: true,
+      data: user,
+      message: "Successfull sign in",
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Something went wrong",
+      data: {},
+      success: false,
+      err: error,
+    });
+  }
+};
 
-const isAuthenticated = async (req,res) => {
-    try {
-        const token = req.headers['x-access-token'];
-        const response = await userService.isAuthenticated(token);
-        return res.status(200).json({
-            success:true,
-            data:response,
-            message: 'User is Authenticated and token is valid',
-            err:{}
-        })
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            message: 'Something went wrong',
-            data:{},
-            success:false,
-            err:error
-        });
-    }
-}
+const isAuthenticated = async (req, res) => {
+  try {
+    const token = req.headers["x-access-token"];
+    const response = await userService.isAuthenticated(token);
+    return res.status(200).json({
+      success: true,
+      data: response,
+      message: "User is Authenticated and token is valid",
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Something went wrong",
+      data: {},
+      success: false,
+      err: error,
+    });
+  }
+};
 
-const isAdmin = async (req,res) => {
-    try {
-        
-        const response = await userService.isAdmin(req.body.id);
-        return res.status(200).json({
-            success:true,
-            data:response,
-            message: 'Successfully fetched whether user is admin or not',
-            err:{}
-        })
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            message: 'Something went wrong',
-            data:{},
-            success:false,
-            err:error
-        });
-    }
-}
+const isAdmin = async (req, res) => {
+  try {
+    const response = await userService.isAdmin(req.body.id);
+    return res.status(200).json({
+      success: true,
+      data: response,
+      message: "Successfully fetched whether user is admin or not",
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Something went wrong",
+      data: {},
+      success: false,
+      err: error,
+    });
+  }
+};
 
 module.exports = {
-    create,
-    destory,
-    get,
-    signIn,
-    isAuthenticated,
-    isAdmin
-}
+  create,
+  destory,
+  get,
+  signIn,
+  isAuthenticated,
+  isAdmin,
+};
