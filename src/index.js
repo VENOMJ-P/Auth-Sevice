a6a5fff1555e5a8127f2aca52289891f3d8f2f3c
VENOMJ-P <jay63837@gmail.com>
2023-08-22 19:15:17 +0530

    Added roles and User_Role

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {PORT} = require('./config/serverConfig');
+
+const {PORT,DB_SYNC} = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 const app=express();
+
+const db = require('./models/index');
 
 const serverConfigSetup = (req,res) => {
     app.use(bodyParser.json());
@@ -12,6 +15,9 @@ const serverConfigSetup = (req,res) => {
     
     app.listen(PORT,(req,re)=>{
         console.log(`Server started at ${PORT}`);
+        if(DB_SYNC==true){
+            db.sequelize.sync({alert:true});
+        }
     })
 }
 
